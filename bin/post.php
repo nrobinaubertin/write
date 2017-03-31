b65450160991f995a559e3fd2232f09b646c4419
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2017-03-31 11:21:21 +0200

    Add a locateFont function

--- bin/post.php
+++ bin/post.php
@@ -16,6 +16,45 @@ function getMetadata($mardown) {
     return $metadata;
 }
 
+function searchFile($file, $dir, $root_path) {
+    while(!file_exists($dir.$file)) {
+        if (realpath($dir) == realpath($root_path)) {
+            break;
+        }
+        $dir = $dir."../";
+    }
+
+    if (file_exists($dir.$file)) {
+        return $dir.$file;
+    } else {
+        return "";
+    }
+}
+
+function locateFont($font, $dir, $root_path) {
+    $formats = ["woff2", "woff", "ttf", "otf"];
+
+    // if the extension is given, serve this format only
+    if (in_array(pathinfo($font, PATHINFO_EXTENSION), $formats)) {
+        $location = searchFile($font, $dir, $root_path);
+        if (file_exists($location)) {
+            return 'url("'.$root_path.'/'.$location.'")';
+        } else {
+            return "";
+        }
+    }
+
+    // if the extension is not given, try different formats
+    foreach($formats as $ext) {
+        $location = searchFile($font.'.'.$ext, $dir, $root_path);
+        if (file_exists($location)) {
+            return 'url("'.$root_path.'/'.$location.'")';
+        }
+    }
+
+    return "";
+}
+
 function genPostHTML($dir, $root_path) {
     
     foreach(scandir($dir) as $file) {
@@ -35,20 +74,20 @@ function genPostHTML($dir, $root_path) {
     $html .= '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">';
 
     if(isset($metadata["title"])) {
-        $html .= '<title>' . $metadata['title'] . '</title>';
-        $html .= '<meta property="og:title" content="' . $metadata["title"] . '">';
+        $html .= '<title>'.$metadata['title'].'</title>';
+        $html .= '<meta property="og:title" content="'.$metadata["title"].'">';
     }
 
     if(isset($metadata["description"])) {
-        $html .= '<meta property="og:description" content="' . $metadata["description"] . '">';
+        $html .= '<meta property="og:description" content="'.$metadata["description"].'">';
     }
 
     if(isset($metadata['title-font'])) {
-        $html .= '<style>@font-face{font-family:"TitleFont";src:url("' . $root_path."/".$dir.$metadata['title-font'] . '");} h1,h2,h3,h4,h5,h6{font-family: "TitleFont", serif;}</style>';
+        $html .= '<style>@font-face{font-family:"TitleFont";src:'.locateFont($metadata['title-font'], $dir, $root_path).';} h1,h2,h3,h4,h5,h6{font-family: "TitleFont", serif;}</style>';
     }
     
     if(isset($metadata['text-font'])) {
-        $html .= '<style>@font-face{font-family:"TextFont";src:url("' . $root_path."/".$dir.$metadata['text-font'] . '");} p{font-family: "TextFont", serif;}</style>';
+        $html .= '<style>@font-face{font-family:"TextFont";src:'.locateFont($metadata['text-font'], $dir, $root_path).';} p{font-family: "TextFont", serif;}</style>';
     }
 
 	$html .= '<style>';
