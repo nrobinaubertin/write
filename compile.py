6979c95bee6d6c5c3cacdb45e409ec6ccd02d706
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-09-21 10:50:52 +0200

    Fixes

--- compile.py
+++ compile.py
@@ -4,41 +4,31 @@ import os, sys, mistletoe, re, shutil
 
 def compilePosts(targetDir, rootDir, distDir):
     rootDir = os.path.abspath(rootDir)
-    if not os.path.isdir(rootDir):
-        sys.exit(rootDir + " is not an existing directory.")
-    if not os.path.isdir(distDir):
-        os.mkdir(distDir)
+    if not os.path.isdir(rootDir): sys.exit(rootDir + " is not an existing directory.")
+    if not os.path.isdir(distDir): os.mkdir(distDir)
     for filename in os.listdir(rootDir + os.sep + targetDir):
-        target = os.path.abspath(rootDir + os.sep + targetDir + os.sep + filename)
-        if os.path.isdir(target):
+        if os.path.isdir(rootDir + os.sep + targetDir + os.sep + filename):
             compilePosts(targetDir + os.sep + filename, rootDir, distDir + os.sep + filename)
             continue
         basename, ext = os.path.splitext(filename)
         if ext == ".md":
             with open(distDir + os.sep + basename + ".html", "w") as distFile:
-                distFile.write(genPostHTML(target))
+                distFile.write(genPostHTML(rootDir + os.sep + targetDir + os.sep + filename))
         else:
-            shutil.copyfile(target, distDir + os.sep + filename)
-
-def parseStaticMarkdown(filename):
-    return mistletoe.markdown(open(filename, "r"))
+            shutil.copyfile(rootDir + os.sep + targetDir + os.sep + filename, distDir + os.sep + filename)
 
 def locateFile(filename, target_dir):
-    target_dir = os.path.abspath(target_dir)
     while (os.path.isdir(target_dir) and not os.path.isfile(os.path.join(target_dir, filename))):
         target_dir = os.path.dirname(target_dir)
-        if target_dir == "/":
-            return ""
-    p = os.path.abspath(os.path.join(target_dir, filename))
-    return p
+        if target_dir == "/": return ""
+    return os.path.abspath(os.path.join(target_dir, filename))
 
 def getRelativePath(path1, path2):
     path1 = os.path.abspath(path1)
     path2 = os.path.abspath(path2)
     c = len(os.path.commonpath([path1, path2]).split(os.sep))
     ll = [".." * (len(path1.split(os.sep)) - c)]
-    p = "/".join(ll + list(path2.split(os.sep)[c:])).strip("/")
-    return p
+    return "/".join(ll + list(path2.split(os.sep)[c:])).strip("/")
 
 def getMetadata(file):
     metadata = {}
@@ -63,27 +53,21 @@ def genCoverImageHTML(metadata):
     coverPicture += '</div>';
     return coverPicture
 
-def genMeta(metadata):
+def genPostHTML(target):
+    path = os.path.abspath(target)
+    if not os.path.exists(path): return ""
+    target_dir = os.path.dirname(path)
+    metadata = getMetadata(path)
+
+    html = "<!DOCTYPE html><html><head>"
     html = '<meta charset="utf8">'
     html += '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">';
     html += '<meta property="og:type" content="article">'
     if "title" in metadata:
         html += '<title>'+metadata["title"]+'</title>'
         html += '<meta property="og:title" content="'+metadata["title"]+'">'
-    if "description" in metadata:
-        html += '<meta property="og:description" content="'+metadata["description"]+'">'
-    if "cover-image" in metadata:
-        html += '<meta property="og:image" content="'+metadata["cover-image"]+'">';
-    return html
-
-def genPostHTML(target):
-    path = os.path.abspath(target)
-    if not os.path.exists(path):
-        return "";
-    target_dir = os.path.dirname(path)
-    metadata = getMetadata(path)
-    html = "<!DOCTYPE html><html><head>"
-    html += genMeta(metadata)
+    if "description" in metadata: html += '<meta property="og:description" content="'+metadata["description"]+'">'
+    if "cover-image" in metadata: html += '<meta property="og:image" content="'+metadata["cover-image"]+'">'
 
     if "title-font" in metadata:
         font = locateFile(metadata["title-font"], target_dir)
@@ -97,13 +81,13 @@ def genPostHTML(target):
         cssFile = locateFile(metadata["style-file"], target_dir)
     else:
         cssFile = locateFile("default.css", target_dir)
-
     html += '<link rel="stylesheet" href="'+getRelativePath(target_dir, cssFile)+'"/>'
+
     html += '</head><body>'
-    if "cover-image" in metadata:
-        html += genCoverImageHTML(metadata)
+
+    if "cover-image" in metadata: html += genCoverImageHTML(metadata)
     html += '<main><article>'
-    html += parseStaticMarkdown(path)
+    html += mistletoe.markdown(open(path, "r"))
     html += '</article></main>'
 
     if "script-file" in metadata:
@@ -111,7 +95,8 @@ def genPostHTML(target):
     else:
         scriptFile = locateFile("default.js", target_dir)
     html += '<script src="'+getRelativePath(target_dir, scriptFile)+'"></script>'
+
     html += '</body></html>'
-    return html;
+    return html
 
 compilePosts("", sys.argv[1], sys.argv[2])
