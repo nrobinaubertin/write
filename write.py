28622d326a1b460c881af314674ff984eaaede39
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-10-03 22:26:58 +0200

    Change some camelCase variables to snake_case

--- write.py
+++ write.py
@@ -3,28 +3,28 @@
 import os, sys, mistletoe, re, shutil
 
 
-def compilePosts(targetDir, rootDir, distDir):
+def compilePosts(target_dir, rootDir, dist_dir):
     rootDir = os.path.abspath(rootDir)
     if not os.path.isdir(rootDir):
         sys.exit(rootDir + " is not an existing directory.")
-    if not os.path.isdir(distDir):
-        os.mkdir(distDir)
-    for filename in os.listdir(rootDir + os.sep + targetDir):
-        if os.path.isdir(rootDir + os.sep + targetDir + os.sep + filename):
+    if not os.path.isdir(dist_dir):
+        os.mkdir(dist_dir)
+    for filename in os.listdir(rootDir + os.sep + target_dir):
+        if os.path.isdir(rootDir + os.sep + target_dir + os.sep + filename):
             compilePosts(
-                targetDir + os.sep + filename, rootDir, distDir + os.sep + filename
+                target_dir + os.sep + filename, rootDir, dist_dir + os.sep + filename
             )
             continue
         basename, ext = os.path.splitext(filename)
         if ext == ".md":
-            with open(distDir + os.sep + basename + ".html", "w") as distFile:
-                distFile.write(
-                    genPostHTML(rootDir + os.sep + targetDir + os.sep + filename)
+            with open(dist_dir + os.sep + basename + ".html", "w") as dist_file:
+                dist_file.write(
+                    genPostHTML(rootDir + os.sep + target_dir + os.sep + filename)
                 )
         else:
             shutil.copyfile(
-                rootDir + os.sep + targetDir + os.sep + filename,
-                distDir + os.sep + filename,
+                rootDir + os.sep + target_dir + os.sep + filename,
+                dist_dir + os.sep + filename,
             )
 
 
@@ -61,24 +61,24 @@ def getMetadata(file):
 
 
 def genCoverImageHTML(metadata):
-    coverPicture = (
+    cover_picture = (
         '<div class="cover"><div class="cover-img" style="background-image:url('
         + metadata["cover-image"]
         + ')"></div>'
     )
     if "cover-credit-url" in metadata and "cover-credit-title" in metadata:
-        coverPicture += (
+        cover_picture += (
             '<a class="credit-badge" href="'
             + metadata["cover-credit-url"]
             + '" target="_blank" rel="noopener noreferrer">'
         )
-        coverPicture += '<span><svg viewBox="0 0 32 32">'
-        coverPicture += '<path d="M20.8 18.1c0 2.7-2.2 4.8-4.8 4.8s-4.8-2.1-4.8-4.8c0-2.7 2.2-4.8 4.8-4.8 2.7.1 4.8 2.2 4.8 4.8zm11.2-7.4v14.9c0 2.3-1.9 4.3-4.3 4.3h-23.4c-2.4 0-4.3-1.9-4.3-4.3v-15c0-2.3 1.9-4.3 4.3-4.3h3.7l.8-2.3c.4-1.1 1.7-2 2.9-2h8.6c1.2 0 2.5.9 2.9 2l.8 2.4h3.7c2.4 0 4.3 1.9 4.3 4.3zm-8.6 7.5c0-4.1-3.3-7.5-7.5-7.5-4.1 0-7.5 3.4-7.5 7.5s3.3 7.5 7.5 7.5c4.2-.1 7.5-3.4 7.5-7.5z"></path>'
-        coverPicture += (
+        cover_picture += '<span><svg viewBox="0 0 32 32">'
+        cover_picture += '<path d="M20.8 18.1c0 2.7-2.2 4.8-4.8 4.8s-4.8-2.1-4.8-4.8c0-2.7 2.2-4.8 4.8-4.8 2.7.1 4.8 2.2 4.8 4.8zm11.2-7.4v14.9c0 2.3-1.9 4.3-4.3 4.3h-23.4c-2.4 0-4.3-1.9-4.3-4.3v-15c0-2.3 1.9-4.3 4.3-4.3h3.7l.8-2.3c.4-1.1 1.7-2 2.9-2h8.6c1.2 0 2.5.9 2.9 2l.8 2.4h3.7c2.4 0 4.3 1.9 4.3 4.3zm-8.6 7.5c0-4.1-3.3-7.5-7.5-7.5-4.1 0-7.5 3.4-7.5 7.5s3.3 7.5 7.5 7.5c4.2-.1 7.5-3.4 7.5-7.5z"></path>'
+        cover_picture += (
             "</svg></span><span>" + metadata["cover-credit-title"] + "</span></a>"
         )
-    coverPicture += "</div>"
-    return coverPicture
+    cover_picture += "</div>"
+    return cover_picture
 
 
 def genPostHTML(target):
@@ -119,11 +119,11 @@ def genPostHTML(target):
         )
 
     if "style-file" in metadata:
-        cssFile = locateFile(metadata["style-file"], target_dir)
+        css_file = locateFile(metadata["style-file"], target_dir)
     else:
-        cssFile = locateFile("default.css", target_dir)
+        css_file = locateFile("default.css", target_dir)
     html += (
-        '<link rel="stylesheet" href="' + getRelativePath(target_dir, cssFile) + '"/>'
+        '<link rel="stylesheet" href="' + getRelativePath(target_dir, css_file) + '"/>'
     )
 
     html += "</head><body>"
@@ -135,10 +135,10 @@ def genPostHTML(target):
     html += "</article></main>"
 
     if "script-file" in metadata:
-        scriptFile = locateFile(metadata["script-file"], target_dir)
+        script_file = locateFile(metadata["script-file"], target_dir)
     else:
-        scriptFile = locateFile("default.js", target_dir)
-    html += '<script src="' + getRelativePath(target_dir, scriptFile) + '"></script>'
+        script_file = locateFile("default.js", target_dir)
+    html += '<script src="' + getRelativePath(target_dir, script_file) + '"></script>'
 
     html += "</body></html>"
     return html
